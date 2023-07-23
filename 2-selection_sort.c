b29c3d02b231e72b27216c079f2614c5157e88d3
Red-Rim <salmaouakhir20@gmail.com>
2023-07-23 10:30:24 -0700

    Selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -13,14 +13,14 @@ void selection_sort(int *array, size_t size)
 	size_t i, j;
 	int min, tmp;
 
-	if (array == NULL || size < 2);
+	if (array == NULL || size < 2)
 		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
 		min = i;
 
-	for (j = i + 1; j < size; j++);
+	for (j = i + 1; j < size; j++)
 	{
 		if (array[j] < array[min])
 		min = j;
