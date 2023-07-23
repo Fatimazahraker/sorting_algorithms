130531c1583d986f8bd142362ca54d8ccddde154
Red-Rim <salmaouakhir20@gmail.com>
2023-07-23 11:13:55 -0700

    Insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -12,7 +12,7 @@ void insertion_sort_list(listint_t **list)
 	listint_t *liist;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
-		return ;
+		return;
 
 	liist = (*list)->next;
 	while (liist)
