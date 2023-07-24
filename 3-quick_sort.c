02067115768676e618d65679f11336404ac237ab
Red-Rim <salmaouakhir20@gmail.com>
2023-07-24 03:47:55 -0700

    Quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -73,7 +73,7 @@ void quick_recursive(int *array, int beginning, int end, size_t size)
  */
 void quick_sort(int *array, size_t size)
 {
-	if (array == NULL || size < 2)
+	if (array == NULL || size <= 1)
 		return;
 
 	quick_recursive(array, 0, size - 1, size);
